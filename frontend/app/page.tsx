6f63358f4c902eac21b10430c6cc8d2bbb4c6ae8
Feras <affi-firas@hotmail.com>
2025-11-07 22:07:50 +0100

    Added the frontend nextjs project

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <> 
+      <main className={styles.main}>
+        <h1 className={styles.header}>Welcome to Learnify</h1>
+      </main>
+    </>
+    
+  );
+}
